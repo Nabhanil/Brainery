5aab83b7e06b8c95819a988ef42b32b46ee49d98
Nabhanil <nabhanilchakraborty084@gmail.com>
2025-01-26 18:40:24 +0530

    first commit-  Backend files added to the github and git repo initialised

---
+++ brainly-backend/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import z from 'zod'
+import bcrypt from 'bcrypt'
+
+const app = express()
+
+app.post('api/v1/signup',async (req,res)=>{
+    
+})
+
+
+app.post('api/v1/signin',async (req,res)=>{
+
+})
+
+app.post('api/v1/content',async (req,res)=>{
+
+})
+
+app.get('api/v1/fetch-content',async (req,res)=>{
+
+})
+app.post('api/v1/delete-content',async (req,res)=>{
+
+})
+
+app.post('api/v1/share', async (req,res)=>{
+
+})
+
+app.get('api/v1/brain/:shareid',async(req,res)=>{
+    
+})
